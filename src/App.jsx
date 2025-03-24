dd7f4d6ee1ae16ae5bd433d7194df8bebf7d40f9
Shambhavi Goyal <sgoyal3@buffalo.edu>
2025-03-24 15:49:51 -0400

    added bg img, added components, and made it better using css

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,13 @@
-// import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css'
+import Main from "./components/Main.jsx"
+import Navbar from "./components/Navbar.jsx/"
 
-function ReactFacts() {
-  return(
-    <main>
-      <img src={reactLogo} width="40px" alt="React logo" />
-      <h1>Fun facts about React</h1>
-        <ul>
-          <li>Was first released in 2013</li>
-          <li>Was originally created by Jordan Walke</li>
-          <li>Has well over 200K stars on Github</li>
-          <li>Is maintained by Meta</li>
-          <li>Powers thousands of enterprise apps, including mobile apps</li>
-        </ul>
-    </main>
-  ); 
+function App(){
+    return(
+        <>
+        <Navbar />
+        <Main />        
+        </>
+    )
 }
 
-export default ReactFacts;
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+function Navbar(){
+    return(
+    <header>
+        <nav>
+            <img src="src\react-logo.png" alt="react logo" />
+            <span><b>React Facts</b></span>
+        </nav>
+    </header>
+    )
+}
+
+export default Navbar;
